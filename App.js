32bb699b7e141b21b4ae3495867da086ef3921ec
GarryChoudhary <gggaurav2210@gmail.com>
2025-02-13 01:28:47 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+let Questions=[
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"},
+    {question:"What is h1 in HTML",options:["Hyper Transfer Markup Language","Hyper Text Markup Language","High Tech Markup Language","Hyperlink Text Managing Language"],answers:"Hyper Text Markup Language"}
+];
+
+let count=1;
+function OnNext() {
+    //   document.getElementById("QuestionNo").innerHTML="2";
+      console.log("clicked");
+};
